d67cd3051aac52d12734986d15281a63f43ea72d
sun0511 <sun0511777@gmail.com>
2019-10-24 02:40:36 +0900

    final

--- test3/urls.py
+++ test3/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from django.urls import re_path, path
-from graphene_django.views import GraphQLView
 from test3.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^graphql$', GraphQLView.as_view(graphiql=True, schema=schema))
-
 ]
